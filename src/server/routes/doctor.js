f1609727e0622bf998ca517fc0152be6dd938871
Mgs. Tabrani <63233752+mgstabrani@users.noreply.github.com>
2022-01-01 16:23:20 +0700

    feature-fix: resolve conflict (#12)

--- src/server/routes/doctor.js
+++ src/server/routes/doctor.js
@@ -4,11 +4,8 @@ module.exports = function doctorRouter(express, doctorController) {
   router.get('/', doctorController.getAllDoctor);
   router.get('/:id', doctorController.getDoctorById);
   router.post('/', doctorController.createDoctor);
-<<<<<<< HEAD
   router.put('/', doctorController.updateDoctor);
   router.delete('/', doctorController.deleteDoctor);
-=======
->>>>>>> development
 
   return router;
 };
